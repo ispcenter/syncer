a33c6c549d508cd5410ffda0ebc6f0339e6d1f9e
icenter <57789326+ispcenter@users.noreply.github.com>
2021-03-05 16:41:22 +0300

    Create Arr.ino

---
+++ Arr.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  char alfabet[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+//  char window[5] = {}
+//  for (i=1; i<){
+    
+//  }
+  Serial.begin(9600);
+//  Serial.println(alfabet.length);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
